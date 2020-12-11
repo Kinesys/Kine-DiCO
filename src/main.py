e80e264aed6636f5bb12093e5cc7eed4869ea9e8
Kinesys <67867993+Kinesys@users.noreply.github.com>
2020-12-11 12:33:37 +0900

    Update main.py

--- src/main.py
+++ src/main.py
@@ -5,26 +5,24 @@ import asyncio
 client = discord.Client()
 
 @client.event
-
-    async def_on_ready():
-       
-        print(client.user.name)
-        print(client.user.id)
-        print("Kinesys Bot_ready")
+async def on_ready():
+    print(client.user.name)
+    print(client.user.id)
+    print("Kinesys Bot_ready")
     
     game = discord.Game("Kinesys Server")
 
     await client.change_presence(status = discord.status.idle, activity = game)
 
 @client.event
-    async def on_message(message):
+async def on_message(message):
 
         if message.author.bot:
             return None
         if message.content.startswith("hi"):
 
             channel = message.channel
-            
-                await channel.send("Hi Kinesys")
+
+        await channel.send("Hi Kinesys")
 
     client.run("token")
