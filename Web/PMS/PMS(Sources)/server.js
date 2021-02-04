8e69abfadce889e3c6d6dfa1d86faeadfdd70543
Kinesys <67867993+Kinesys@users.noreply.github.com>
2021-02-05 07:14:22 +0900

    Update server.js

--- Web/PMS/PMS(Sources)/server.js
+++ Web/PMS/PMS(Sources)/server.js
@@ -1,1 +1,23 @@
+app.set("views", __dirname + "/views");
+app.set("view engine", "ejs");
+app.engine("html", require("ejs").renderFile);
+app.use(express.static("static"));
+app.use(session({
+    secret: config.secret,
+    resave: false,
+    saveUninitialized: true
+}));
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use("/", router_index);
+app.use("/profile", router_profile);
+
+db.registerAdmin(config.ADMIN_ID, config.FLAG)
+    .then((user) => {}, (err) => {
+        console.log("Falied to save admin : ", err);
+    });
+
+const server = app.listen(3000, function() {
+    console.log("Started");
+});
