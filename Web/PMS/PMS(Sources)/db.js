efe4187efcdad7aad571f586fe52ba29eb969a25
Kinesys <67867993+Kinesys@users.noreply.github.com>
2021-02-05 07:13:40 +0900

    Update db.js

--- Web/PMS/PMS(Sources)/db.js
+++ Web/PMS/PMS(Sources)/db.js
@@ -1,1 +1,10 @@
+const config = require("../config");
+const memcached = require("memcached");
+const crypto = require("crypto");
 
+var mc = new memcached;
+
+mc.connect(config.MC_HOST, function(err, conn) {
+    if (err)
+        console.log("Error while constructing connection");
+});
